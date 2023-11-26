50565a41a782d842b0b21ebdca2c26ddd3d43ed5
toby <toby@swinson-howell.net>
2023-11-26 13:02:01 +0000

    fix?

--- src/scripts/discuss.js
+++ src/scripts/discuss.js
@@ -2,7 +2,7 @@ var link = document.createElement('li');
 link.innerHTML = '<a href="/discuss">Discuss</a>';
 link.className = 'link discuss';
 try {
-    document.querySelector('div#navigation div.inner ul')
+    document.querySelector('nav#navigation div.inner ul')
         .insertBefore(link, document.querySelector('div#navigation div.inner ul').getElementsByTagName('li')[3]);
 } catch (err) {
     document.querySelector('div#topnav ul.site-nav')
